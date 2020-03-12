d48161036eeb6e7b462ae90d838df288e6a65451
Tom <metaphorps@gmail.com>
2020-03-11 21:57:34 -0500

    finishing basic CRUD

--- app.js
+++ app.js
@@ -2,36 +2,18 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express(); // calling express adds a bunch of methods to our variable we save it to
+const port = 3000;
+const devDataToursSimplePath = `${__dirname}/dev-data/data/tours-simple.json`;
+// JSON.parse will auto convert an array of JS objects
+const tours = JSON.parse(fs.readFileSync(devDataToursSimplePath));
 
 // we need this, try commenting it out, it changes client req body json to a req object, without it, its undefined
 // app.use allows us to pass it middleware to use to enhance experience, streamline request data, etc, augment http data handling, serverside, for example
-app.use(express.json()); // express.json is middleware. middleware modifies or enhances data, usually incoming requests // middleware stands in middle between req and response
 // express.json means that the body is added to the request object
+app.use(express.json()); // express.json is middleware. middleware modifies or enhances data, usually incoming requests // middleware stands in middle between req and response
 
-const port = 3000; //
-
-// routing is telling the app what to do with different request, and also the request type, get, post etc
-// this get method is only called when the base url or / url is sent a get request
-// app.get('/', (req, res) => {
-//     // res.status(200).send(`Hello from the server side!`); // sends string response
-//     // json method is great to send a response in json format
-//     res.status(200).json({
-//         message: 'Hello from the bloody server',
-//         app: `listening on port ${port}`
-//     }); // sends json response
-// });
-
-// arg1 is client url
-// good to specify api versions
-// const tours = require('./dev-data/data/tours-simple.json')
-// JSON.parse will auto convert an array of JS objects
-const devDataToursSimplePath = `${__dirname}/dev-data/data/tours-simple.json`;
-const tours = JSON.parse(
-    fs.readFileSync(devDataToursSimplePath)
-);
-
-// get tour list by client
-app.get('/api/v1/tours', (req, res) => {
+// get all tours
+const getAllTours = (req, res) => {
     // send all tours
     // usually send status and data, which is the "envelope" which holds our data
     res.status(200).json({
@@ -44,7 +26,36 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     });
-});
+};
+// get specific tour by id
+const getTour = (req, res) => {
+    // console.log(req.params); // all url variables are stored here in params object
+    const id = req.params.id * 1; // JS weirdly converts strings that look like numbers to actual numbers
+    const tour = tours.find(el => el.id === id); // find method stores an array of els that match existing condition aka it loops through tours element ids to match one to the param id
+    console.log(id);
+
+    // if (id >= tours.length) {
+    if (!tour) {
+        console.log(`No good, tour ${id} is undefined`);
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tour
+        }
+    });
+};
+
+app.get('/api/v1/tours', getAllTours);
+
+// define variable using a colon
+// however, you may be missing one or more variables, then make those optional with a '?' after it ie: '/api/v1/tours/:id/:name?/:type?'
+app.get('/api/v1/tours/:id', getTour);
 
 // create new tour by client
 // in postman, you can customize the request data, by: "Body" tab => "raw" => "JSON"(from dropdown)
@@ -53,7 +64,8 @@ app.post('/api/v1/tours', (req, res) => {
     const newID = tours[tours.length - 1].id + 1;
     // merge objects, new id, and req data sent
     // Object.assign allows us to create new obj from merging two different objects together-- notice arg1 is target object, arg2 is source object pulling values from to copy into arg1 object
-    const newTour = Object.assign({
+    const newTour = Object.assign(
+        {
             id: newID
         },
         req.body
@@ -64,7 +76,7 @@ app.post('/api/v1/tours', (req, res) => {
     console.log(tours[tours.length - 1]); // log last tour added
 
     // persist new user-added tour locally
-    // JSON stringify to 
+    // JSON stringify to
     // callbacks are basically functions that run when the method called is complete
     fs.writeFile(devDataToursSimplePath, JSON.stringify(tours), err => {
         // send response, newly created object back to client
@@ -74,7 +86,7 @@ app.post('/api/v1/tours', (req, res) => {
             data: {
                 data: newTour
             }
-        })
+        });
     });
 });
 
@@ -82,6 +94,46 @@ app.post('/', (req, res) => {
     res.send(`You can post to this URL`);
 });
 
+// PUT expects the entire updated obj, whereas PATCH expects only the properties that will be updated on the object
+// just like POST,  you need to include body content in raw JSON
+app.patch('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    // if (id >= tours.length) {
+    if (req.params.id * 1 >= tours.length) {
+        console.log(`No good, tour ${id} is undefined`);
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated tour here...'
+        }
+    });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id;
+    // if (id >= tours.length) {
+    if (req.params.id * 1 >= tours.length) {
+        console.log(`No good, tour ${id} is undefined`);
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+    // notice statuscode for delete is 204, which means 'no content', also we send null as res
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+});
+
+// turn server on
 app.listen(port, (req, res) => {
     console.log(`App running on port ${port}`);
 });
