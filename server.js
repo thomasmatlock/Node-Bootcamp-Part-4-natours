73874e48b5e498f3fe46864e19320db282a036bc
Tom <metaphorps@gmail.com>
2020-04-03 19:05:00 -0500

    finished 8.84, creating simple schema and model for mongoose

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ dotenv.config({
     path: './config.env'
 });
 
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 // console.log(DB);
 
 // mongoose.connect returns a promise, so we can use .then(), then() gets access to the connection obj
@@ -29,8 +26,33 @@ mongoose
 // change environment: in terminal, preface your 'npm start'/'nodemon server.js' with 'NODE_ENV=production' or 'NODE_ENV=development', then check logged env vars, it will show it as changed
 // to change more, use 'NODE_ENV=development X=23 npm start' and it will update accordingly
 
-// START SERVER
+// we create mongoose models out of mongoose schema: we use schema to describe value, define default fields and values, validate data, etc
+// pass in schema as an object
+// in the scheme obj, we can do it simply, or add options, like required=true, etc
+// we also can add an error handler (looks ternary ) for some options: instead of true, use an array containing successful and failure values
+// MONGOOSE SCHEMA
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name.'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price.']
+    }
+});
 
+// MONGOOSE MODEL
+// mongoose convention to always use uppercase on models
+// arg1 = name of model, arg2 = schema of model
+const Tour = mongoose.model('Tour', tourSchema);
+
+// START SERVER
 const port = process.env.PORT || 3000;
 
 app.listen(port, (req, res) => {
