8501256234a3cd21592b821ab3ae243c16ef33ad
Tom <metaphorps@gmail.com>
2020-04-07 23:30:05 -0500

    tiny refa

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); // pass morgan predefined string (6 options or so) to define how we want our req object to look when its console logged
 }
 app.use(express.json()); // express.json is middleware. middleware modifies or enhances data, usually incoming requests // middleware stands in middle between req and response
-// these are available in browser, as long as full path is there/ partial paths like /img dont work, cuz it thinks its a route
 app.use(express.static(`${__dirname}/public`)); // this makes available static files, then the folder they are located in
 
 app.use((req, res, next) => {
@@ -21,17 +20,19 @@ app.use((req, res, next) => {
 });
 
 // MOUNTED ROUTERS
-app.use('/api/v1/tours', tourRouter); // we want arg2 to handle everything sent to arg1, the endpoint. args: endpoint, router to handle it
-app.use('/api/v1/users', userRouter); // we want arg2 to handle everything sent to arg1, the endpoint. args: endpoint, router to handle it
+app.use('/api/v1/tours', tourRouter); // arg1 = URL endpoint, arg2 = router handling requests sent to arg1 endpoint
+app.use('/api/v1/users', userRouter); // arg1 = URL endpoint, arg2 = router handling requests sent to arg1 endpoint
 
 module.exports = app; // export to server.js for use
 
+////////////////////////////////////////////////////////////////
 //  FLOW:
 // request enter via app.js, then gets passed to one of the 2 routers we mounted to handle them
 // the routers then run the controllers attached to them
 // app.js => mounted routers => router file => controller file
 // its good practice to separate express and server.js file
 
+////////////////////////////////////////////////////////////////
 // we need this, try commenting it out, it changes client req body json to a req object, without it, its undefined
 // app.use allows us to pass it middleware to use to enhance experience, streamline request data, etc, augment http data handling, serverside, for example
 // express.json means that the body is added to the request object
@@ -43,6 +44,7 @@ module.exports = app; // export to server.js for use
 // just like POST,  you need to include body content in raw JSON
 // app.route chaining only works for methods that share same root, ie no params or w/e
 
+////////////////////////////////////////////////////////////////
 // https://expressjs.com/en/resources/middleware.html for all middlewares you can use and that express recommends
 // https://expressjs.com/en/4x/api.html to check out all methods available for res/req etc
 // routes are also middleware, they only turn on in specified cases, URL matches arg1 etc
@@ -53,6 +55,7 @@ module.exports = app; // export to server.js for use
 // we can call next whatever we want, but arg3 is accepts as the next method
 // console.log(`${__dirname}/../dev-data`); // you get cwd, go up 1 folder, and select what you want. use consolelog to print some these, its easy
 
+////////////////////////////////////////////////////////////////
 // MOUNTING A ROUTER (using a new router as middleware on an existing route to handle it, then using prexisting routing to handle sub routing)
 // all routers are running off of the 'app' object. to split routing into multiple files for cleaner code, we need to create a router for each of the resources
 // there will be 4 files, 2 handlers for users and tours, 2 routers for users and tours, then a router to handle all them
@@ -60,6 +63,10 @@ module.exports = app; // export to server.js for use
 // this arg2 router, in turn, has its own routes
 // basically when a req comes in, if it matches arg1, the endpoint, it will run the arg2, the router in charge of handling w/e comes to that endpoint
 // now we split the files, basically we created small apps, imported them, and mounted them on the ENDPOINTS (api/v1/tours|users) to handle requests to those endpoints
-
+////////////////////////////////////////////////////////////////
 // 65, env variables
 // most important environments are dev env, and production env
+
+////////////////////////////////////////////////////////////////
+// eslint-disable-next-line prettier/prettier
+////////////////////////////////////////////////////////////////
--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv'); // npm package that handles our config.env file, useful to toggle between production and development
 
-// reads our config.env file and save them to nodejs env variable
 dotenv.config({
     path: './config.env'
 });
 const app = require('./app');
 
-
-// mongoose.connect returns a promise, so we can use .then(), then() gets access to the connection obj
-// we name it 'con' for connection, and its the results of the returned promise
-// arg1, db connection string; arg2, obj that is some options to deal w deprecation warnings
-// dont worry about the 4 arg2 properties right now. make them exactly the same in my own projects
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose
     .connect(DB, {
@@ -21,14 +15,30 @@ mongoose
         useUnifiedTopology: true
     })
     .then(() => console.log(`DB connection successful!`));
+
+const port = process.env.PORT || 3000; // START SERVER
+
+app.listen(port, (req, res) => {
+    console.log(`App running on port ${port}`);
+});
+
+////////////////////////////////////////////////////////////////
+// reads our config.env file and save them to nodejs env variable
+// dotenv.config({
+//     path: './config.env'
+// });
+
+////////////////////////////////////////////////////////////////
+// mongoose.connect returns a promise, so we can use .then(), then() gets access to the connection obj
+// we name it 'con' for connection, and its the results of the returned promise
+// arg1, db connection string; arg2, obj that is some options to deal w deprecation warnings
+// dont worry about the 4 arg2 properties right now. make them exactly the same in my own projects
+
+////////////////////////////////////////////////////////////////
 // console.log(app.get('env')); // shows us whether in dev or production env. default is dev
 // console.log(process.env); // WOW THIS IS THE SHIT! look at all the  environment vars we have to use for Warp!
 // change environment: in terminal, preface your 'npm start'/'nodemon server.js' with 'NODE_ENV=production' or 'NODE_ENV=development', then check logged env vars, it will show it as changed
 // to change more, use 'NODE_ENV=development X=23 npm start' and it will update accordingly
 
-// START SERVER
-const port = process.env.PORT || 3000;
-
-app.listen(port, (req, res) => {
-    console.log(`App running on port ${port}`);
-});
+// eslint-disable-next-line prettier/prettier
+////////////////////////////////////////////////////////////////
